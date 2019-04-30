39ddbd3c6c618fdd19deb3c3e1dba1217ee5e4fc
chulhee23 <chulhee23@naver.com>
2019-04-30 18:07:37 +0900

    posts show

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts':posts})
