aab037c47eb44fc73ebb6e05d2fcfeffc539b525
chulhee23 <chulhee23@naver.com>
2019-04-30 18:30:32 +0900

    create 'create' method

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from django.utils import timezone
 # Create your views here.
 def home(request):
     posts = Post.objects.all()
@@ -8,3 +9,14 @@ def home(request):
 def detail(request,post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'detail.html', {'post':post})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_post = Post()
+    new_post.title=request.POST['title']
+    new_post.writer = request.POST['writer']
+    new_post.pub_date = timezone.datetime.now()
+    new_post.save()
+    return redirect('home')
--- lion_class/urls.py
+++ lion_class/urls.py
@@ -20,5 +20,7 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
-    path('post/<int:post_id>', blog.views.detail, name="detail")
+    path('post/<int:post_id>', blog.views.detail, name="detail"),
+    path('new/', blog.views.new, name='new'),
+    path('create/', blog.views.create, name='create'),
 ]
