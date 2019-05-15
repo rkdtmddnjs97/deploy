ecdd4fb6228b6da5148c24b297423e1b0fb2c405
chulhee23 <chulhee23@naver.com>
2019-05-15 17:17:22 +0900

    url

--- lion_class/settings.py
+++ lion_class/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lion_class.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['lion_class/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lion_class/urls.py
+++ lion_class/urls.py
@@ -1,29 +1,9 @@
-"""lion_class URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
-    path('post/<int:post_id>', blog.views.detail, name="detail"),
-    path('new/', blog.views.new, name='new'),
-    path('create/', blog.views.create, name='create'),
-    path('edit/<int:post_id>', blog.views.edit, name="edit"),
-    path('update/<int:post_id>', blog.views.update, name="update"),
-    path('delete/<int:post_id>', blog.views.delete, name="delete"),
+    path('post/', include('blog.urls')),
 ]
